f37dd2d4e14d0b42d24678f2a8376f2e300d2460
senn59 <74777070+senn59@users.noreply.github.com>
2023-12-09 20:49:47 +0100

    implement process order, did some refactoring

--- friture/Program.cs
+++ friture/Program.cs
@@ -1,9 +1,11 @@
 ﻿using friture;
 
-var snacks = new List<Snack>();
-snacks.Add(new Snack("Frikandel", 1.50m, 20));
-snacks.Add(new Snack("Kipnuggets", 1.20m, 25));
-snacks.Add(new Snack("Croket", 1.80m, 24));
+var snacks = new List<Snack>
+{
+    new Snack("Frikandel", 1.50m, 20),
+    new Snack("Kipnuggets", 1.20m, 25),
+    new Snack("Croket", 1.80m, 24)
+};
 var snackBar = new SnackBar(snacks);
 
 //OPTIONS 
@@ -13,7 +15,7 @@ const int stock = 3;
 
 while (true) {
     Console.Clear();
-    Console.WriteLine("What would you like to do? \n 1: Order \n 2: Check revenue \n 3: Check stock"); //aanpassing op meerdere plekken
+    Console.WriteLine("What would you like to do? \n 1: Order \n 2: Check revenue \n 3: Check stock");
 
     var option = Console.ReadLine();
     if (option == null) continue;
@@ -27,20 +29,19 @@ while (true) {
 
             foreach (var snack in snackBar.SnackList)
             {
-                int amount = PromptAmount(snack);
+                var amount = PromptOrderAmount(snack);
                 clientOrder.Add((snack, amount));
             }
-            //var priceToPay = snackBar.ProcessOrder(clientOrder);
-            //Console.WriteLine($"Your total is: ${priceToPay}");
+            var amountDue = snackBar.ProcessOrder(clientOrder);
+            Console.WriteLine($"Your total is: ${amountDue}");
             break;
         case revenue:
             Console.WriteLine(snackBar.TotalRevenue);
             break;
         case stock:
-            var snackStock = snackBar.GetStock();
-            foreach (var snack in snackStock)
+            foreach (var snack in snackBar.SnackList)
             {
-                Console.WriteLine(snack);
+                Console.WriteLine($"{snack.Name}: {snack.AmountInStock}");
             }
             break;
         default:
@@ -51,20 +52,20 @@ while (true) {
     Console.ReadLine();
 }
 
-int PromptAmount(Snack snack)
+int PromptOrderAmount(Snack snack)
 {
     Console.WriteLine($"How many of {snack} would you like to order? ({snack.AmountInStock} in stock)");
     var amount = Console.ReadLine();
-    if (!int.TryParse(amount, out int parsedAmount))
+    if (!int.TryParse(amount, out var parsedAmount))
     {
-        PromptAmount(snack);
+        PromptOrderAmount(snack);
     }
 
     var result = snack.CanOrder(parsedAmount);
     if (!result.Ok)
     {
         Console.WriteLine(result.Message);
-        PromptAmount(snack);
+        PromptOrderAmount(snack);
     }
 
     return parsedAmount;
--- friture/SnackBar.cs
+++ friture/SnackBar.cs
@@ -2,31 +2,21 @@ namespace friture;
 
 public class SnackBar
 {
-    private readonly List<Snack> snackList;
+    private List<Snack> snackList;
     public decimal TotalRevenue;
     public IReadOnlyList<Snack> SnackList => snackList.AsReadOnly();
+    
     public SnackBar(List<Snack> snackList)
     {
         this.snackList = snackList;
     }
-    public decimal ProcessOrder()
+    public decimal ProcessOrder(List<(Snack, int)> order)
     {
-        //TODO: implement
-        return 0.0m;
-    }
-    
-    public List<string> GetSnackNames()
-    {
-        return snackList.Select(snack => snack.Name).ToList();
-    }
-
-    public List<(string, int)> GetStock()
-    {
-        var stockPerItem = new List<(string, int)>();
-        foreach (var snack in snackList)
+        decimal amountDue = 0;
+        foreach (var (snack, amount) in order)
         {
-            stockPerItem.Add((snack.Name, snack.AmountInStock));
+            amountDue += snack.Order(amount);
         }
-        return stockPerItem;
+        return amountDue;
     }
 }
