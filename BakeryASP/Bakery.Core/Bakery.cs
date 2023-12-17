fcb78606cee110bb39701e9a59ac8da610e8f80d
senn59 <74777070+senn59@users.noreply.github.com>
2023-12-17 15:55:45 +0100

    implement base Bakery class

--- BakeryASP/Bakery.Core/Bakery.cs
+++ BakeryASP/Bakery.Core/Bakery.cs
@@ -2,5 +2,51 @@ namespace Bakery.Core;
 
 public class Bakery
 {
+    private const int VatPercentage = 9;
+    public string Name { get; private set; }
+    private double _revenue = 0;
+    private List<Sandwich> _sandwiches = new List<Sandwich>();
+    public IReadOnlyList<Ingredient> Ingredients = new List<Ingredient>([
+        new Ingredient("lettuce", 0.2),
+        new Ingredient("tomato", 0.3),
+        new Ingredient("ham", 0.25),
+    ]).AsReadOnly();
+
+    public Bakery(string name)
+    {
+        this.Name = name;
+    }
+
+    public void AddSandwich(Sandwich sandwich)
+    {
+        _sandwiches.Add(sandwich);
+    }
+
+    public IReadOnlyList<Sandwich> GetAvailableSandwiches()
+    {
+        return _sandwiches.AsReadOnly();
+    }
     
+    public IReadOnlyList<Sandwich> GetAvailableSandwiches(BreadType bread)
+    {
+        var filteredSandwiches = _sandwiches.Where(s => s.Bread == bread).ToList();
+        return filteredSandwiches.AsReadOnly();
+    }
+
+    public double SellSandwich(Sandwich sandwich)
+    {
+        var price = sandwich.GetPrice();
+        _revenue += price;
+        return CalculateTotalWithVat(price);
+    }
+
+    public double CalculateRevenue(bool includeVat)
+    {
+        return includeVat ? CalculateTotalWithVat(_revenue) : _revenue;
+    }
+
+    private double CalculateTotalWithVat(double total)
+    {
+        return total + total * (VatPercentage / 100.0);
+    }
 }
