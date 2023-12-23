fedd55b4ccce3fe5f6856df8fecb8581075fbe36
senn59 <senn3004@gmail.com>
2023-12-23 22:49:54 +0100

    add some js to track order

--- BakeryASP/BakeryASP/wwwroot/js/site.js
+++ BakeryASP/BakeryASP/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let orderEl;
+document.onreadystatechange = () => {
+    orderEl = document.getElementById("order");
+}
+const addToOrder = (sandwich) => {
+    console.log(sandwich)
+    const sandwichOrderCount = document.querySelector(`input[name='${sandwich}']`)
+    sandwichOrderCount.value++;
+    const orderTxt = `${sandwich} x${sandwichOrderCount.value}`;
+    const orderItem = document.querySelector(`#order [id='${sandwich}']`)
+    console.log(orderItem)
+    if (orderItem != null) {
+        orderItem.innerText = orderTxt
+    } else {
+        const html = `
+            <div id="${sandwich}">
+                ${orderTxt}
+            </div>
+        `
+        orderEl.innerHTML += html;
+    }
+}
