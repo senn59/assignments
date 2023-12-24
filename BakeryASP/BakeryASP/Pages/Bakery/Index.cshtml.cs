b5dbe65019ccef1b0335becca67ab7f675559402
senn59 <74777070+senn59@users.noreply.github.com>
2023-12-24 14:51:14 +0100

    changed how we display cart info, now able to get customer order in post request

--- BakeryASP/BakeryASP/Pages/Bakery/Index.cshtml.cs
+++ BakeryASP/BakeryASP/Pages/Bakery/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Bakery.Core;
 using BakeryASP.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BakeryASP.Pages.Bakery;
@@ -10,17 +11,33 @@ public class Index : PageModel
     public string BakeryName { get; private set; }
     public IReadOnlyList<Sandwich> Sandwiches { get; private set; }
     public List<String> Breads { get; set; } = Enum.GetNames<BreadType>().ToList();
+    [BindProperty] public Dictionary<string, int> Cart { get; set; } = new Dictionary<string, int>();
     private readonly BakeryService _bakeryService;
     
     public Index(BakeryService bakeryService)
     {
         this._bakeryService = bakeryService;
         this.Sandwiches = _bakeryService.Bakery.GetAvailableSandwiches();
+        foreach (var s in this.Sandwiches)
+        {
+            Cart.Add(s.Name, 0);
+        }
         this.BakeryName = _bakeryService.Bakery.Name;
     }
     
     public void OnGet()
     {
         
+    }
+
+    public IActionResult OnPost()
+    {
+        foreach (var item in Cart) 
+        {
+            Console.WriteLine(item.Key);
+            Console.WriteLine(item.Value);
+        }
+
+        return RedirectToPage("/Bakery/Index");
     }
 }
