d3364befcef5b17f429f700c9060aa2dd6ad7d89
senn59 <74777070+senn59@users.noreply.github.com>
2024-01-10 14:51:19 +0100

    Add order DTO and cleanup bakery

---
+++ BakeryASP/Bakery.Core/Services/OrderService.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+using Bakery.Core.Dtos;
+
+namespace Bakery.Core.Services;
+
+public class OrderService
+{
+    public void Insert(OrderDto order)
+    {
+        string filePath = "order-data.json";
+        string orderContents = File.Exists(filePath) ? File.ReadAllText(filePath) : "[]";
+        List<OrderDto>? orders = JsonSerializer.Deserialize<List<OrderDto>>(orderContents) ?? new List<OrderDto>();
+
+        orders.Add(order);
+
+        var orderContentsNew = JsonSerializer.Serialize(orders);
+        File.WriteAllText(filePath, orderContentsNew);
+    }
+
+    public IEnumerable<OrderDto> GetAll()
+    {
+        string filePath = "order-data.json";
+        if (!File.Exists(filePath)) return [];
+        string orderContents = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<List<OrderDto>>(orderContents) ?? new List<OrderDto>();
+        
+        // converteer de DTOs naar echte order en order item classes
+        // TotalPrice
+    }
+}
--- BakeryASP/BakeryASP/Pages/Bakery/Index.cshtml.cs
+++ BakeryASP/BakeryASP/Pages/Bakery/Index.cshtml.cs
@@ -1,4 +1,6 @@
 using Bakery.Core;
+using Bakery.Core.Dtos;
+using Bakery.Core.Services;
 using BakeryASP.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,41 +13,86 @@ public class Index : PageModel
     public string BakeryName { get; private set; }
     public IReadOnlyList<Sandwich> Sandwiches { get; private set; }
     public List<string> Breads { get; set; } = Enum.GetNames<BreadType>().ToList();
+
     [BindProperty] public Dictionary<string, int> Cart { get; set; } = new Dictionary<string, int>();
+    public double revenue { get; set; } = 0;
+
     private readonly BakeryService _bakeryService;
-    
-    public Index(BakeryService bakeryService)
+    private readonly OrderService _orderService;
+    private readonly TimeProvider _timeProvider;
+
+    public Index(BakeryService bakeryService, OrderService orderService, TimeProvider timeProvider)
     {
         this._bakeryService = bakeryService;
+        _orderService = orderService;
+        _timeProvider = timeProvider;
         this.Sandwiches = _bakeryService.Bakery.GetAvailableSandwiches();
         foreach (var s in this.Sandwiches)
         {
             Cart.Add(s.Name, 0);
         }
+
         this.BakeryName = _bakeryService.Bakery.Name;
     }
-    
+
     public void OnGet()
     {
-        
+        var orders = _orderService.GetAll();
+        foreach (var order in orders)
+        {
+            foreach (var sandwich in order.Sandwiches)
+            {
+                this.revenue += sandwich.SandwichPrice * sandwich.Amount;
+            }
+        }
     }
 
     public IActionResult OnPost()
     {
-        // var convertedCart = new Dictionary<Sandwich, int>();
-        foreach (var item in Cart)
+        var orders = new List<OrderItemDto>();
+        foreach (var item in Cart.Where(kvp => !kvp.Key.Contains("Request")))
         {
             var sandwich = this.Sandwiches.FirstOrDefault(s => s.Name == item.Key);
-                Console.WriteLine(item);
             if (sandwich == null)
             {
-                Console.WriteLine("is null");
-                // Console.WriteLine(item.Key);
-                // ModelState.AddModelError(nameof(Cart), "Invalid sandwich in cart.");
-                // return Page();
+                ModelState.AddModelError(nameof(Cart), "Invalid sandwich in cart.");
+                return Page();
             }
-            // convertedCart.Add(sandwich, item.Value);
+
+            if (item.Value == 0)
+            {
+                continue;
+            }
+
+            orders.Add(new OrderItemDto
+            {
+                SandwichName = sandwich.Name,
+                SandwichPrice = sandwich.GetPrice(),
+                Amount = item.Value
+            });
         }
+
+        // List<OrderItemDto> orders = Cart
+        //     .Where(kvp => !kvp.Key.Contains("Request"))
+        //     .Select(kvp =>
+        //     {
+        //         var sandwich = this.Sandwiches.FirstOrDefault(s => s.Name == kvp.Key) ??
+        //                        throw new InvalidOperationException("This should not happen. Hackerz?");
+        //
+        //         return new OrderItemDto
+        //         {
+        //             SandwichName = sandwich.Name,
+        //             SandwichPrice = sandwich.GetPrice(),
+        //             Amount = kvp.Value
+        //         };
+        //     }).ToList();
+
+        // _bakeryService.Bakery.Order(order);
+        _orderService.Insert(new OrderDto
+        {
+            Sandwiches = orders,
+            DateCreated = _timeProvider.GetUtcNow().DateTime
+        });
 
         return RedirectToPage("/Bakery/Index");
     }
