478c656b5e069898a6a9e517fe4ecd553c5f8958
senn59 <74777070+senn59@users.noreply.github.com>
2024-05-24 16:04:18 +0200

    update

--- s2/ContainerTransport/ContainerTransport.UI/Program.cs
+++ s2/ContainerTransport/ContainerTransport.UI/Program.cs
@@ -1,11 +1,11 @@
 ﻿using ContainerTransport.Core;
 
 var generator = new ContainerGenerator();
-var ship = new Ship(4, 6);
+var ship = new Ship(7, 7);
 var containers = generator.GenerateRandomContainers(ship);
 ship.Load(containers, true);
 Console.WriteLine(ship.Balance);
 Console.WriteLine(UrlGenerator.GenerateFrom(ship));
-ship.Load(containers, false);
-Console.WriteLine(ship.Balance);
-Console.WriteLine(UrlGenerator.GenerateFrom(ship));
+// ship.Load(containers, false);
+// Console.WriteLine(ship.Balance);
+// Console.WriteLine(UrlGenerator.GenerateFrom(ship));
